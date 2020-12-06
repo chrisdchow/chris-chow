6901cd57172e53159e378eabcfbe61720237f048
Chris Chow <chris@ivalice.local>
2020-12-05 23:11:02 -0800

    update static content and formatting
    - change font

--- components/home.tsx
+++ components/home.tsx
@@ -18,28 +18,33 @@ export const Home: FunctionComponent<HomeProps> = ({ posts }): JSX.Element => (
     <Head>
       <title>{siteTitle}</title>
     </Head>
-    <section className={utilStyles.headingMd}>
-      <p>[Your Self Introduction]</p>
-      <p>
-        (This is a sample website - you’ll be building a site like this on{' '}
-        <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
-      </p>
-    </section>
-    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-      <h2 className={utilStyles.headingLg}>Blog</h2>
-      <ul className={utilStyles.list}>
-        {posts.map(({ id, date, title }) => (
-          <li className={utilStyles.listItem} key={id}>
-            <Link href='/posts/[id]' as={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
-        ))}
-      </ul>
-    </section>
+    <div className='space-y-6'>
+      <section>
+        <p>
+          Philadelphia born and raised software engineer currently based in San
+          Francisco. You may view my resume{' '}
+          <a href='/christopher-chow-resume.pdf' target='_blank'>
+            here
+          </a>
+          .
+        </p>
+      </section>
+      <section>
+        <h2 className=''>Posts</h2>
+        <ul className='list-none space-y-4'>
+          {posts.map(({ id, date, title }) => (
+            <li className='' key={id}>
+              <Link href='/posts/[id]' as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
   </Layout>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-  },
   purge: [
     // Use *.tsx if using TypeScript
     './pages/**/*.tsx',
